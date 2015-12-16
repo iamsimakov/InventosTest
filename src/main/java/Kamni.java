500a0d4f6df1bb1838399b4209ea2ce0433ac29f
alexey.simakov <jimsim@mail.ru>
2015-12-16 08:35:40 +0300

    Kamni

--- src/main/java/Kamni.java
+++ src/main/java/Kamni.java
@@ -1,8 +1,10 @@
 import java.io.*;
-import java.util.Arrays;
-
 
 public class Kamni {
+
+    private static int saveDiff;
+    private static int sum;
+
     public static void main(String[] args) throws Exception {
         StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
         in.nextToken();
@@ -12,27 +14,65 @@ public class Kamni {
             in.nextToken();
             mas[j] = (int)in.nval;
         }
-        Arrays.sort(mas);
-        int sum = 0;
+
+        for (int i = 0; i < mas.length; i++){
+            for (int j = 0; j < mas.length; j++){
+                if (mas[i] < mas[j]){
+                    int a = mas[i];
+                    mas[i] = mas[j];
+                    mas[j] = a;
+                }
+            }
+        }
+
+        sum = 0;
         for (int i : mas) {
             sum += i;
         }
-        int saveDiff = sum;
-        int saveHeapSum = 0;
-        for (int i = 0; i < mas.length; i++) {
-            int heap1Sum = mas[i];
+        saveDiff = sum;
+        double sered = sum / 2.0d;
+        for (int j=0; j<mas.length; j++){
+            int firstHeap = mas[j];
+            if ( ((double)firstHeap) > sered) {
+                calcDiff(firstHeap);
+                continue;
+            }
             for (int k = 0; k < mas.length; k++) {
-                int diff = Math.abs(heap1Sum - (sum - heap1Sum));
-                if (diff < saveDiff) {
-                    saveDiff = diff;
-                    saveHeapSum = heap1Sum;
+                firstHeap = mas[j];
+                for (int i = k; i < mas.length; i++) {
+                    if (i != j) {
+                        firstHeap += mas[i];
+                        if ( ((double) firstHeap) > sered) {
+                            calcDiff(firstHeap, mas[i]);
+                            break;
+                        }
+                    }
                 }
-                if (i == k) continue;
-                heap1Sum += mas[k];
             }
         }
         System.out.println(saveDiff);
-        System.out.println(saveHeapSum);
+
+    }
+
+    public static void calcDiff(int a){
+        int diff = Math.abs(a - (sum - a) );
+        if (diff < saveDiff){
+            saveDiff = diff;
+        }
+    }
+
+    public static void calcDiff(int a, int b){
+        int diffWithB = Math.abs(a - (sum - a) );
+        int diffWithoutB = Math.abs( a - b - (sum - (a - b) ) );
+        if (diffWithB < diffWithoutB){
+            if (diffWithB < saveDiff){
+                saveDiff = diffWithB;
+            }
+        } else {
+            if (diffWithoutB < saveDiff){
+                saveDiff = diffWithoutB;
+            }
+        }
     }
 
 }
